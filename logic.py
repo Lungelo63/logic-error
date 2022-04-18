cf1e1cb36c892abe356abe53369847d3f11e1bd0
Lungelo Packery <81719132+Lungelo63@users.noreply.github.com>
2022-04-18 23:25:22 +0200

    Add files via upload

---
+++ logic.py
@@ -0,0 +1,5 @@
+movies = 6              #assigning variables to int and str
+autobots = "the good guys"
+decept = "Megatron"     #decept is a variable but it a bad choice
+
+print("There are " + autobots + " movies and " + str(movies) + " " + decept + " lead by Decepticons")   #The codes prints out whats it's told but the structure of the sentence is wrong.
